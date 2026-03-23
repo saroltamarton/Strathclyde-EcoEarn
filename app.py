0dd3012e256ebbc3a07dc78f35ffcfa304ea9051
Sarolta Marton <sarolta.marton@gmail.com>
2026-03-23 09:15:40 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 import streamlit as st
 
-st.set_page_config(page_title="Test App")
-
-st.title("Test App")
-st.write("The app is working.")
+st.title("Test app")
+st.write("If you can see this, it is working.")
